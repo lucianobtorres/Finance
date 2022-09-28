0f552b6bdda20cd8fe54b0b78385531b5765e33d
lbtorres <lbtorres@tecgraf.puc-rio.br>
2022-09-28 12:40:56 -0300

    [V01]  - Criação inicial da APP;

--- PWA-Finance/src/app/app.module.ts
+++ PWA-Finance/src/app/app.module.ts
@@ -3,13 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { HomeComponent } from './components/home/home.component';
+import { MaterialModule } from './module/material.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ PWA-Finance/src/app/components/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Todo {
+  desc!: string;
+}
+
+@Component({
+  selector: 'fi-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  todo: Todo = new Todo();
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  save(todo: Todo) {
+    this.todos.push(todo);
+    this.todo = new Todo();
+    this.todos = Object.assign([], this.todos);
+  }
+  delete(todo: Todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
+  }
+
+}
